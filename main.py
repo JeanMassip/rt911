c17944a382306fa6dfb0657a8267a45b500c4e3c
Jean Massip <jean.massip@yahoo.com>
2023-05-04 14:33:04 +0200

    Modification timeout

--- main.py
+++ main.py
@@ -16,34 +16,35 @@ async def main():
   BatteryUsage = 80
 
   while True:
-    emit = threading.Thread(target=advertise, args=(myID, BatteryUsage, Threshold, 40,))
+    emit = threading.Thread(target=advertise, args=(myID, BatteryUsage, Threshold, 10,))
     print("Start emitting...")
+    emit.daemon = True
     emit.start()
 
     print("Start discovering...")
-    devices = await BleakScanner.discover(timeout=40)
+    devices = await BleakScanner.discover(timeout=10)
     for d in devices:
         if d.name == 'BJPT':
-            data = d.details['props']['ManufacturerData'][0xffff]
-            id = data[0]
-            battery = data[1]
-            if battery >= Threshold:
-                if id < LeaderID:
-                    LeaderID = id
-                    LeaderName = d.address
-                    Threshold = data[2]
-                    print("Threshold is: " + str(Threshold))
+          data = d.details['props']['ManufacturerData'][0xffff]
+          id = data[0]
+          battery = data[1]
+          if battery >= Threshold:
+            if id < LeaderID:
+              LeaderID = id
+              LeaderName = d.address
+              Threshold = data[2]
+              print("Threshold is: " + str(Threshold))
     
     PreviousLeader == LeaderID
     if LeaderID == myID:
-        BatteryUsage -= 10
-        if PreviousLeader == myID:
-            Threshold -=10
+      BatteryUsage -= 10
+      if PreviousLeader == myID:
+        Threshold -=10
   
   print("The Leader address is : " + str(LeaderName) + " and it's  Id : " + str(LeaderID))
-  time.sleep(30)
+  time.sleep(10)
 
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+  asyncio.run(main())
